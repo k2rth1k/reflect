9c16ae59c999abd3badf379a5603bfb0b8e8ed12
k2rth1k <karthik.nelapati@gmail.com>
2025-06-13 17:51:33 -0400

    added React-routes to navigate different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
 import './App.css';
-import {useEffect, useState} from "react";
-import Button from '@mui/material/Button';
-import {BarChart} from '@mui/x-charts/BarChart';
-import {LineChart} from "@mui/x-charts";
-
-interface chartData {
-    x: number[],
-    y: number[]
-}
+import {RouterProvider} from "react-router";
+import {router} from "./routes/routes";
 
 function App() {
-    const [data, setData] = useState<chartData>({x: [], y: []});
-    useEffect(() => {
-        const xAxis: number[] = [];
-        const yAxis: number[] = [];
-        window.electronAPI.getAllWeeklySets().then((res) => {
-            res.forEach(val => {
-                xAxis.push(val.week_number)
-                yAxis.push(val.sets)
-            })
-            setData({x: xAxis, y: yAxis});
-        });
-    }, []);
 
     return (
-        <div className="App">
-
-            {<LineChart
-                xAxis={[{data: data.x}]}
-                series={[
-                    {
-                        data: data.y,
-                    },
-                ]}
-                height={300}
-            />}
-
-            <Button variant="contained">Hello world</Button>
-            <BarChart
-                xAxis={[{data: ['group A', 'group B', 'group C']}]}
-                series={[{data: [4, 3, 5]}, {data: [1, 6, 3]}, {data: [2, 5, 6]}]}
-                height={300}
-            />
-        </div>
+        <>
+            <RouterProvider router={router}/>
+        </>
     );
 }
 
